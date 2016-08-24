614dde74355bc770df20d4acc0e690f250848588
David Gaskin <davidburkegaskin@gmail.com>
2016-08-23 21:21:20 -0700

    Initial Commit
    Start: header, nav, carousel feature display

---
+++ index-template.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<!-- IE Edge Meta tag -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="In floor heating offers many benefits. Learn about hydronic infloor heat from experts serving Toronto and Southern Ontario." />
+		<title>In-Floor Heating systems designed, installed by expert infloor heat contractor in Ontario</title>
+		<!-- Latest compiled and minified Bootstrap CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		<link rel="stylesheet" href="css/main.css">
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	</head>
+	<body>
+		<header>
+			<h1>Canadian In-Floor Radiant Solutions</h1>
+		</header>
+		<nav>
+			<h2>Navigation</h2>
+			<ul>
+				<li><a href="index.html">Home</a></li>
+				<li><a href="aboutUs.html">About Us</a></li>
+				<li><a href="contact.html">Contact</a></li>
+				<li><a href="infloorHeat.html">In Floor Heat</a>
+					<ul>
+						<li><a href="infloorHeat.html#design">Design</a></li>
+						<li><a href="infloorHeat.html#system_components">System Components</a></li>
+						<li><a href="infloorHeat.html#installation">Installation</a></li>
+						<li><a href="infloorHeat.html#costs">Costs</a></li>
+						<li><a href="infloorHeat.html#maintenance_and_repairs">Maintenance &amp; Repairs</a></li>
+						<li><a href="infloorHeat.html#lifespan_and_warranty">Lifespan &amp; Warranty</a></li>
+						<li><a href="infloorHeat.html#benefits">Benefits</a></li>
+						<li><a href="infloorHeat.html#cautions_and_solutions">Cautions and Solutions</a></li>
+					</ul>
+				</li>
+				<li><a href="thermalMass.html">Thermal Mass</a></li>
+				<li><a href="snowMelt_HeatedDriveways.html">Snow Melt &amp; Heated Driveways</a></li>
+				<li><a href="fireSafetySystems.html">Fire Safety Systems</a></li>
+				<li><a href="poolHeating.html">Pool Heating</a></li>
+				<li><a href="whatDoesItCost.html">What Does It Cost?</a>
+					<ul>
+						<li><a href="whatDoesItCost.html#example1">Concrete Basement Floor of Existing Home</a></li>
+						<li><a href="whatDoesItCost.html#example3">Full Basement During New Home Construction (1500 sq/ft)</a></li>
+						<li><a href="whatDoesItCost.html#example4">Entire New Home Including Basement (4000 sq/ft)</a></li>
+						<li><a href="whatDoesItCost.html#example5">Underfloor Heating on Main Floor of Existing Home</a></li>
+					</ul>
+				</li>
+			</ul>
+		</nav>
+		<main>
+			<section>
+				<h2>Radiant Heating Systems for Your Home</h2>
+				<h3>An Allergy-Free Way to Stay Warm</h3>
+				<p>
+					Warm your home with a highly-efficient, draft, dust and allergy-free radiant heating system. If you're building a new home or renovating an existing one and you're in the Niagara, Hamilton/Burlington, Greater Toronto Area or in cottage country in Ontario <a href="tel:1234567896">talk to us</a> about the possibility of installing a radiant heating system.
+					<!-- NOTE: verify proper usage of phone link -->
+				</p>
+				<p>
+					New to the concept of radiant heating systems? We invite you to take a few moments to visit our <a href="">In Floor Heating Page</a> for a brief overview.
+				</p>
+				<p>
+					Hydronic radiant heating systems are excellent for home heating but they can also be used to keep your driveway and walks clear of snow and ice in the winter. To learn more about these options check out our page on <a href="">Heated Driveways</a>.
+				</p>
+				<footer>
+					<h4>Partners</h4><!-- NOTE: What are these exactly?? -->
+					<!-- Partners Logo Container -->
+					<div>
+						<a href="http://www.ultimatecomfort.ca/" rel="external"><img src="#" alt="Logo: Canadian Hydronics Council" title="Canadian Hydronics Council"></a>
+						<a href="http://www.burlingtonchamber.com/" rel="external"><img src="#" alt="Logo: Burlington Chamber of Commerce" title="Burlington Chamber of Commerce"></a>
+						<a href="http://www.oakvillechamber.com/" rel="external"><img src="#" alt="Logo: Oakville Chamber of Commerce" title="Oakville Chamber of Commerce"></a>
+						<a href="https://www.facebook.com/beautifulheat/" rel="external"><img src="#" alt="Logo: Beautiful Heat" title="Beautiful Heat"></a><!-- old inactive URL: http://www.beautifulheat.com/home replaced with Facebook page :: Revisit page later on -->
+					</div>
+				</footer>
+			</section>
+			<aside>
+				<h3>Uponor Accreditation</h3>
+				<header>
+					<img src="#" alt="Logo: Uponor"><!-- Add uponor image here -->
+					<p>
+						The world leader in radiant materials has accredited us with level 2 certification.
+					</p>
+				</header>
+				<p>
+					This special qualification entitles you to a <strong>25 Year Manufacturer's Warranty</strong> on systems we install
+				</p>
+			</aside>
+		</main>
+		<footer>
+			<h2>Footer</h2>
+			<!-- NOTE: How to include a second navigation in footer SEMANTICALLY?? -->
+			<p>
+				&copy; 2016 Canadian In-Floor Radiant Solutions
+			</p>
+			<!-- TODO: Add a careers page -->
+			<!-- TODO: Consider applying ASP.net server scripting for includes -->
+		</footer>
+		<!-- jQuery Core -->
+		<script src="https://code.jquery.com/jquery-3.0.0.min.js" integrity="sha256-JmvOoLtYsmqlsWxa7mDSLMwa6dZ9rrIdtrrVYRnDRH0=" crossorigin="anonymous"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>The Garage Door Depot - Canada's #1 Garage Door Company</title>
+		<meta name="description" content="Canada's national overhead door company. Let us help choose the right style and design for your new garage door design." />
+		<link rel="canonical" href="http://www.garagedoordepot.ca/"/>
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="css/main.css">
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	</head>
+	<body class="container-fluid">
+		<header class="container">
+
+			<!-- Company Logo -->
+			<div id="main_logo"></div>
+
+			<!-- Contact # -->
+			<div id="contact_number">
+				<span>Call&nbsp;Us:&nbsp;(250)&nbsp;885-5455</span>
+			</div>
+		</header>
+
+		<!-- Main Nav -->
+		<nav>
+			<ul class="container">
+				<li><a href="#">Residential&nbsp;Doors</a></li>
+				<li><a href="#">Commercial&nbsp;Doors</a></li>
+				<li><a href="#">Other&nbsp;Products</a></li>
+				<li class="active"><a href="#">Company&nbsp;Information</a></li>
+			</ul>
+		</nav>
+		<!-- Breadcrumbs -->
+		<!-- <section id="breadcrumbs" style="background-color: #e4f9d9; height: 35px;">
+		</section> -->
+
+		<main>
+
+			<!-- Feature Window -->
+			<section>
+				<div id="carousel" class="carousel slide" data-ride="carousel">
+					<!-- Indicators -->
+					<ol class="carousel-indicators">
+						<li data-target="#carousel" data-slide-to="0" class="active"></li>
+						<li data-target="#carousel" data-slide-to="1"></li>
+						<li data-target="#carousel" data-slide-to="2"></li>
+						<li data-target="#carousel" data-slide-to="3"></li>
+					</ol>
+
+					<!-- Wrapper for slides -->
+					<div class="carousel-inner" role="listbox">
+
+						<!-- IMAGES GOES HERE -->
+						<div class="item active">
+							<img src="images/multi-color-garage-doors.jpg" alt="Chania">
+						</div>
+
+						<div class="item">
+							<img src="images/wood-garage-door.jpg" alt="Flower">
+						</div>
+
+						<div class="item">
+							<img src="images/resGarageDoors.jpg" alt="Flower">
+						</div>
+					</div>
+
+					<!-- Left and right controls -->
+					<a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
+						<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+						<span class="sr-only">Previous</span>
+					</a>
+					<a class="right carousel-control" href="#carousel" role="button" data-slide="next">
+						<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+						<span class="sr-only">Next</span>
+					</a>
+				</div>
+			</section>
+			<!-- end Feature Window -->
+
+			<!-- Content -->
+			<section id="content-1" class="container">
+				<div class="row">
+					<section class="col-lg-4">
+						1
+					</section>
+					<section class="col-lg-4">
+						2
+					</section>
+					<section class="col-lg-4">
+						3
+					</section>
+				</div>
+
+			</section>
+
+
+
+
+		</main>
+
+
+		<!--jQuery CDN-->
+		<script src="https://code.jquery.com/jquery-3.0.0.min.js" integrity="sha256-JmvOoLtYsmqlsWxa7mDSLMwa6dZ9rrIdtrrVYRnDRH0=" crossorigin="anonymous"></script>
+		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	</body>
+</html>
