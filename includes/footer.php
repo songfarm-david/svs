d32b6ed06687a050b9f44e4623b86b13c0422871
David Gaskin <davidburkegaskin@gmail.com>
2016-08-30 21:16:43 -0700

    Create includes folder; Begin Contact Us page

---
+++ includes/footer.php
@@ -0,0 +1,75 @@
+<!-- Footer -->
+<footer id="footer-primary">
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-4">
+				<h4>Hours &amp; Contact Info</h4>
+					<address class="">
+						<p>
+							<a href="tel:250-885-5455">(250)-885-5455</a>
+						</p>
+						<p>
+							123 W Fake St.
+						</p>
+						<p>
+							Victoria, British Columbia
+						</p>
+						<p>
+							V2Z 1H0
+						</p>
+						<p>
+							<small>Available through the week 9am-5pm.
+							<br>
+							Weekend service available.</small>
+						</p>
+					</address>
+			</div>
+			<div class="col-lg-4">
+				<article>
+					<a href="#">
+						<h4>Franchise Opportunities</h4>
+					</a>
+					<p>
+						Carrot cake dragée lollipop chocolate cake sugar plum toffee halvah carrot cake jelly. Chocolate bar pie candy canes halvah halvah tart liquorice sweet roll. Marzipan jelly-o jelly-o. Pie jelly <a href="#">beans carrot</a> cake caramels brownie.
+					</p>
+					<a href="#" class="cta-franchise">Contact: Mr. John Smith</a>
+				</article>
+			</div>
+			<div class="col-lg-4">
+				<h4>Connect with Us!</h4>
+				<ul class="social-links">
+					<li><a href="#"><span class="fa fa-facebook-official"></span></a></li>
+					<li><a href="#"><span class="fa fa-twitter-square"></span></a></li>
+					<li><a href="#"><span class="fa fa-google-plus-square"></span></a></li>
+					<li><a href="#"><span class="fa fa-linkedin-square"></span></a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</footer>
+<section class="container-fluid copyright">
+	<p>
+		<small>&copy; Garage Door Depot Victoria - 2016</small>
+	</p>
+</section>
+
+<!--jQuery CDN-->
+<script src="https://code.jquery.com/jquery-3.0.0.min.js" integrity="sha256-JmvOoLtYsmqlsWxa7mDSLMwa6dZ9rrIdtrrVYRnDRH0=" crossorigin="anonymous"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script src="https://use.fontawesome.com/85510088e9.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+<script>
+      function initMap() {
+        var mapDiv = $("#map > div");
+				// console.log(mapDiv[0]);
+        var map = new google.maps.Map(mapDiv[0], {
+            center: {lat: 44.540, lng: -78.546},
+            zoom: 8
+        });
+      }
+    </script>
+    <script async defer
+        src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBqQYqpFlKldgk3QcDlfWgmc9kHRcjfedU&callback=initMap">
+    </script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Bob's Widgets - #1 in Victoria!</title>
+		<meta name="description" content="Your site description here.." />
+		<link rel="canonical" href="http://www.garagedoordepot.ca/"/>
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="css/main.css">
+		<link href="https://fonts.googleapis.com/css?family=Asap:400,700|Noto+Sans:400,700|Nunito:300,400,700" rel="stylesheet">
+		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+	</head>
+	<body class="container-fluid">
+		<!-- Javascript Pre-loader -->
+		<div class="loading-part">
+			<div class="loader-home">Loading...</div>
+		</div>
+
+		<!-- CONTENT START -->
+
+		<!-- Main Header -->
+		<header id="header-main">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 col-sm-6 col-xs-12">
+						<section id="brand-logo-main">
+							<h4 class="hidden">Company Logo</h4>
+							<a href="index.php">
+								<!-- Company Logo Here -->
+								<img src="assets/garage-door-depot-logo.png" alt="Garage Door Depot logo" width="100%"/>
+							</a>
+						</section>
+					</div>
+					<div class="col-md-6 col-sm-6 col-xs-12">
+						<section id="contact-primary">
+							<!-- Main CTA -->
+							<h4 class="hidden">Primary Contact</h4>
+							<a href="tel:1234567895">
+								Call:&nbsp;(123)&nbsp;456-7895
+							</a>
+						</section>
+						<section id="widget-1">
+							<!-- Widget 1 -->
+							<h4 class="hidden">Social Media Links</h4>
+							<a href="#"><span class="fa fa-google-plus-square"></span></a>
+							<a href="#"><span class="fa fa-linkedin-square"></span></a>
+							<a href="#"><span class="fa fa-facebook-official"></span></a>
+						</section>
+					</div>
+				</div>
+			</div>
+		</header>
+		<!-- end of Header -->
+
+		<!-- Main Nav -->
+		<nav>
+			<div class="container-fluid">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#nav-collapse">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+				</div>
+				<div class="container-fluid collapse navbar-collapse" id="nav-collapse">
+					<ul class="container">
+						<li><a href="index.php">Home</a></li>
+						<li class="dropdown-btn">
+							<a href="about-us.php" data-toggle="collapse" data-target=".sub-nav-collapse-1">About Us</a>
+							<div class="collapse navbar-collapse sub-nav-collapse-1" id="sub-nav-collapse">
+								<ul class="dropdown-content">
+									<li><a href="about-us.php#about-us">About Us</a></li>
+									<li><a href="about-us.php#testimonials">Testimonials</a></li>
+								</ul>
+							</div>
+						</li>
+						<li class="dropdown-btn">
+							<a href="services.php" data-toggle="collapse" data-target=".sub-nav-collapse-2">Services</a>
+							<div class="collapse navbar-collapse sub-nav-collapse-2" id="sub-nav-collapse">
+								<ul class="dropdown-content">
+									<li><a href="services.php#service-1">Service 1</a></li>
+									<li><a href="services.php#service-2">Service 2</a></li>
+									<li><a href="services.php#service-3">Service 3</a></li>
+								</ul>
+							</div>
+						</li>
+						<li><a href="contact-us.php">Contact Us</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
